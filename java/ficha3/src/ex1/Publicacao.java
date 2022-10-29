b732140e1c95d63851a12e5f7fb2b31c5b29d4ad
awlaska <sofiassantossousa@gmail.com>
2022-10-29 18:24:19 +0100

    pub e revista

--- java/ficha3/src/ex1/Publicacao.java
+++ java/ficha3/src/ex1/Publicacao.java
@@ -1,15 +1,50 @@
 package ex1;
 
 public class Publicacao {
-    //TODO i. O atributo privado ‘nome’ do tipo String;
-    //TODO ii. A variável de Classe privada ‘quantidadePublicacoes’ do tipo inteiro,
+    //DONE i. O atributo privado ‘nome’ do tipo String;
+    private String nome;
+
+    //DONE ii. A variável de Classe privada ‘quantidadePublicacoes’ do tipo inteiro,
     // que representará o número de Publicações que foram criadas.
-    //TODO iii. O atributo privado ‘numeroPublicacao’ do tipo inteiro.
+    private int quantidadePublicacoes;
+
+    //DONE iii. O atributo privado ‘numeroPublicacao’ do tipo inteiro.
     // Este número pode ser visto como um identificador único.
-    //TODO iv. Um Construtor de Instâncias da Classe.
+    private int numeroPublicacao;
+
+    //DONE iv. Um Construtor de Instâncias da Classe.
     // O construtor deverá igualar o atributo ‘numeroPublicacao’ ao valor da variável da classe ‘quantidadePublicacoes’
     // e depois incrementar a última;
-    //TODO v. Os métodos Getter e Setter;
+    Publicacao(){
+        numeroPublicacao = quantidadePublicacoes;
+        quantidadePublicacoes++;
+    }
+
+    //DONE v. Os métodos Getter e Setter;
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidadePublicacoes() {
+        return quantidadePublicacoes;
+    }
+
+    public void setQuantidadePublicacoes(int quantidadePublicacoes) {
+        this.quantidadePublicacoes = quantidadePublicacoes;
+    }
+
+    public int getNumeroPublicacao() {
+        return numeroPublicacao;
+    }
+
+    public void setNumeroPublicacao(int numeroPublicacao) {
+        this.numeroPublicacao = numeroPublicacao;
+    }
+
     //TODO vi. O método mostraInformacao() que apresenta a informação da publicação;
     //TODO vii. O método introduzirDados(…) que recebe o ‘nome’ para a publicação;
     //TODO viii. O método obterQuantidadePublicacoes() que devolve o número de publicações existentes.
--- java/ficha3/src/ex1/Revista.java
+++ java/ficha3/src/ex1/Revista.java
@@ -1,9 +1,26 @@
 package ex1;
 
 public class Revista extends Publicacao {
-    //TODO i. Os atributos privados ‘nome’ e ‘numeroRevista’ do tipo String;
+    //DONE i. Os atributos privados ‘nome’ e ‘numeroRevista’ do tipo String;
+    private String nome, numeroRevista;
     //TODO ii. Um construtor de Instâncias da Classe;
-    //TODO iii. Os métodos Getter e Setter;
+    //DONE iii. Os métodos Getter e Setter;
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumeroRevista() {
+        return numeroRevista;
+    }
+
+    public void setNumeroRevista(String numeroRevista) {
+        this.numeroRevista = numeroRevista;
+    }
+
     //TODO iv. O método mostraInformacao() que apresenta a informação da Revista;
     //TODO v. O método introduzirDados(…) que recebe o ‘nome’ e o ‘numeroRevista’ para a Revista;
 }
