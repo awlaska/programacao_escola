ad7974432b25df7f0177893c85fa49927aaf5f6a
awlaska <sofiassantossousa@gmail.com>
2022-11-03 18:20:21 +0000

    venda

--- java/ex_PL/src/folha2/ex1_2_3/Venda.java
+++ java/ex_PL/src/folha2/ex1_2_3/Venda.java
@@ -1,13 +1,63 @@
 package folha2.ex1_2_3;
 
+import java.util.Date;
+
 public class Venda {
-    //TODO a) Um atributo privado, de tipo Date, dataVenda, para conter a data da venda.
-    //TODO b) Um atributo privado, de tipo Produto[] (array de Produto), produtos, para conter os produtos vendidos numa venda.
-    //TODO c) Um construtor de instâncias da classe, sem argumentos, que cria uma venda na data de hoje (data do sistema).
-    //TODO d) Os métodos getter dos dois atributos definidos anteriormente (dataVenda e produtos).
-    //TODO e) Um método de instância adicionaProduto() que adiciona um produto à venda.
+    //DONE a) Um atributo privado, de tipo Date, dataVenda, para conter a data da venda.
+    private Date dataVenda;
+
+    //DONE b) Um atributo privado, de tipo Produto[] (array de Produto),
+    // produtos, para conter os produtos vendidos numa venda.
+    private Produto[] produtos;
+
+    //DONE c) Um construtor de instâncias da classe, sem argumentos,
+    // que cria uma venda na data de hoje (data do sistema).
+    public Venda(){
+        Venda v = new Venda();
+        v.dataVenda = new Date();
+    }
+
+    //DONE d) Os métodos getter dos dois atributos definidos anteriormente (dataVenda e produtos).
+    public Date getDataVenda() {
+        return this.dataVenda;
+    }
+
+    public void setDataVenda(Date dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public Produto[] getProdutos() {
+        return this.produtos;
+    }
+
+    public void setProdutos(Produto[] produtos) {
+        this.produtos = produtos;
+    }
+
+    //DOING e) Um método de instância adicionaProduto() que adiciona um produto à venda.
+    public void adicionaProduto(Produto p){
+        Produto pr = new Produto();
+    }
+
     //TODO f) Um método de instância calculaTotalVenda() que retorna o valor total da venda (soma dos preços de venda dos produtos dessa venda).
-    //TODO g) Um método de instância totMargens() que retorna o total de margem de lucro desta venda.
-    //TODO Escreva a função main() para testar a classe Venda e Produto. Crie uma venda com vários produtos com diferentes valores dos seus atributos. Obtenha os valores da venda e margem total.
-    //TODO No projeto anterior, altere a classe Venda, criando um atributo de classe vendas (double) com o respetivo getter (método de classe), e modificando o construtor de forma a contabilizar em vendas o número de vendas efetuadas (numero de instâncias de Venda criadas).
+    public double calculaTotalVenda(){
+        double total = 0.0;
+        for(Produto p : produtos){
+            total = p.getPrecoVenda() + total;
+        }
+        return total;
+    }
+
+    //TODO g) Um método de instância totMargens()
+    // que retorna o total de margem de lucro desta venda.
+
+    //TODO Escreva a função main() para testar a classe Venda e Produto.
+    // Crie uma venda com vários produtos com diferentes valores dos seus atributos.
+    // Obtenha os valores da venda e margem total.
+
+    //TODO No projeto anterior, altere a classe Venda,
+    // criando um atributo de classe vendas (double) com o respetivo getter (método de classe),
+    // e modificando o construtor de forma a contabilizar em vendas o número de vendas efetuadas
+    // (numero de instâncias de Venda criadas).
+
 }
--- java/ex_PL/src/folha2/ex1_2_3/main.java
+++ java/ex_PL/src/folha2/ex1_2_3/main.java
@@ -1,4 +1,8 @@
 package folha2.ex1_2_3;
 
 public class main {
+    public static void main(String[] args) {
+        Venda v = new Venda();
+        System.out.println("data " + v.getDataVenda());
+    }
 }
